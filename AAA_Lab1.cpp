946a52367e5dda59bc3210766dfdd063d1f11fb4
AceAtomz <93909209+AceAtomz@users.noreply.github.com>
2022-08-17 21:27:19 +0200

    OOP

--- AAA_Lab1.cpp
+++ AAA_Lab1.cpp
@@ -2,27 +2,23 @@
 #include <cstring>
 using namespace std;
 
-int main() {
-    string grid[3][3];
-    string inputGrid;
-    string inputString;
-    cin >> inputGrid >> inputString;
-    string outputString;
+string grid[3][3];
+
+void printGrid(string grid[3][3]){
     string outputGrid;
-
-    int blank = inputGrid.find("#");
-
-    //string moves[4] = {"UP", "DOWN", "LEFT", "RIGHT"};
-
-    int counter =0;
-
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            grid[i][j] = inputGrid[counter];
-            counter++;
+            outputGrid += grid[i][j];
+            if(j!=2){
+                outputGrid += " ";
+            }
         }
+        outputGrid += "\n";
     }
+    cout << "1\n" << outputGrid;
+}
 
+void makeMove(string inputString,  int blank){
     if(inputString=="UP" && blank>2){
         string temp1;
         if(blank>5){
@@ -68,18 +64,30 @@ int main() {
             grid[(blank-2)/3][1] = "#";
         }
     }
+}
 
+int main() {
+    string inputGrid;
+    string inputString;
+    cin >> inputGrid >> inputString;
+    string outputString;
+
+    int blank = inputGrid.find("#");
+
+    string moves[4] = {"UP", "DOWN", "LEFT", "RIGHT"};
+
+    int counter =0;
 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            outputGrid += grid[i][j];
-            if(j!=2){
-                outputGrid += " ";
-            }
+            grid[i][j] = inputGrid[counter];
+            counter++;
         }
-        outputGrid += "\n";
     }
 
-    cout << "1\n" << outputGrid;
+    makeMove(inputString, blank);
+
+    printGrid(grid);
     return 0;
 }
+
